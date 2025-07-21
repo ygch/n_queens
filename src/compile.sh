9529436f7e5c4fededb8adffb841b4267c46a512
yaogc <yaogc@xiaopeng.com>
2025-07-21 10:57:43 +0800

    Add runtime for A100 card.

--- src/compile.sh
+++ src/compile.sh
@@ -2,4 +2,7 @@
 
 set -x
 
+# 4090 with arch: compute_89,code=sm_89
+# 5090 with arch: compute_120,code=sm_120
+# A100 with arch: compute_80,code=sm_80
 nvcc -gencode arch=compute_89,code=sm_89 --ptxas-options=-v -allow-unsupported-compiler -I /usr/local/cuda/include -L /usr/local/cuda/lib64 -lcudart n_queens_cuda.cu n_queens.cpp utils.cpp main.cpp -o n_queens -O3 -Xcompiler -fopenmp
