0bebd5177f5332daa558c5376b7451ad0f59370f
yaogc <yaogc@xiaopeng.com>
2025-07-30 14:27:54 +0800

    Add a single H800 result.

--- src/compile.sh
+++ src/compile.sh
@@ -5,4 +5,5 @@ set -x
 # 4090 with arch: compute_89,code=sm_89
 # 5090 with arch: compute_120,code=sm_120
 # A100 with arch: compute_80,code=sm_80
+# H100/H800 with arch: compute_90,code=sm_90
 nvcc -gencode arch=compute_89,code=sm_89 --ptxas-options=-v -allow-unsupported-compiler -I /usr/local/cuda/include -L /usr/local/cuda/lib64 -lcudart n_queens_cuda.cu n_queens.cpp utils.cpp main.cpp -o n_queens -O3 -Xcompiler -fopenmp -D_USE_CONFIG2_
